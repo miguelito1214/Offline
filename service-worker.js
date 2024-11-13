1e6d0fb76eb87672e878009792da0cc3c9db61f2
Jose Miguel <132602041+miguelito1214@users.noreply.github.com>
2024-11-13 09:38:11 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,34 @@
-const CACHE_NAME = 'mi-pwa-cache-v1';  
-const urlsToCache = [  
-'./',  
-'./index.html',  
-'./app.js',  
-'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/c  
-];  
-// Evento de instalación  
-self.addEventListener('install', event => {  
-event.waitUntil(  
-caches.open(CACHE_NAME)  
-.then(cache => {  
-console.log('Abriendo caché');  
-return cache.addAll(urlsToCache);  
-})  
-);  
-});  
-// Evento de activación  
-self.addEventListener('activate', event => {  
-console.log('Service Worker activado');  
-});  
-// Evento de fetch para interceptar solicitudes y servi  
-self.addEventListener('fetch', event => {  
-event.respondWith(  
-caches.match(event.request)  
-.then(response => {  
-return response || fetch(event.request);
-})
-);
+const CACHE_NAME = 'mi-pwa-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+];
+
+// Evento de instalación
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Abriendo caché');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
+
+// Evento de activación
+self.addEventListener('activate', event => {
+  console.log('Service Worker activado');
+});
+
+// Evento de fetch para interceptar solicitudes y servir desde la caché si está disponible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
